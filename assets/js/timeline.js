31b486f9bb5e6e459eab2f781979e2926485c589
cAlonso10 <carlosalonsomunoz@gmail.com>
2024-04-21 16:34:04 +0200

    Se añade el efecto de aparicion a la center-line del roadmap

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,8 +1,9 @@
 console.log("Script cargado");
-var $element=$('.each-event');
+var $element = $('.each-event, .each-event2'); 
 var $window = $(window);
 $window.on('scroll resize', check_for_fade);
 $window.trigger('scroll');
+
 function check_for_fade() { 
     var window_height = $window.height();
     
@@ -10,12 +11,19 @@ function check_for_fade() {
         var $element = $(this);
         var element_height = $element.outerHeight();
         var element_offset = $element.offset().top;
-        space = window_height - (element_height + element_offset -$(window).scrollTop());
-        if (space < 60) {
-            $element.addClass("non-focus");
-        } else {
-            $element.removeClass("non-focus");
+        space = window_height - (element_height + element_offset - $(window).scrollTop());
+        if ($element.hasClass("each-event")) { 
+            if (space < 60) {
+                $element.addClass("non-focus");
+            } else {
+                $element.removeClass("non-focus");
+            }
+        } else if ($element.hasClass("each-event2")) { 
+            if (space < -1190) {
+                $element.addClass("non-focus");
+            } else {
+                $element.removeClass("non-focus");
+            }
         }
- 
     });
-};
+}
